461f2f616d60ea6732f8766feee1d07bb926009f
Miguel Boanova <miguel.boanova@prodigious.com>
2017-10-23 12:52:46 -0200

    :bug:

--- src/components/QrReader.vue
+++ src/components/QrReader.vue
@@ -25,87 +25,96 @@
 </template>
 
 <script>
-import 'webrtc-adapter' // shim getUserMedia
-import { QrcodeReader } from 'vue-qrcode-reader'
-import db from '../firebase'
+import "webrtc-adapter"; // shim getUserMedia
+import { QrcodeReader } from "vue-qrcode-reader";
+import db from "../firebase";
 export default {
   components: { QrcodeReader },
-  data () {
+  data() {
     return {
       points: [],
-      content: 'Carregando imagem...',
+      content: "Carregando imagem...",
       errors: [],
       paused: false,
       pauseOnCapture: true
-    } 
+    };
   },
   methods: {
-    onDecode (content) {
-      this.content = content
-      console.log(db)
-      db.ref('codes').push({'code': content})
+    onDecode(content) {
+      this.content = content;
+      console.log(db);
+      db.ref("codes").push({ code: content });
       if (this.pauseOnCapture) {
-        this.paused = true
+        this.paused = true;
       }
     },
-    onLocate (points) {
-      this.points = points
+    onLocate(points) {
+      this.points = points;
     },
-    onError (message) {
-      this.errors.push(message)
+    onError(message) {
+      this.errors.push(message);
     },
-    onStreamLoaded () {
-      this.content = ''
+    onStreamLoaded() {
+      this.content = "";
     },
-    positionOf ({ x, y }) {
+    positionOf({ x, y }) {
       return {
-        'top': y + 'px',
-        'left': x + 'px'
-      }
+        top: y + "px",
+        left: x + "px"
+      };
     },
-    togglePauseOnCapture () {
-      this.pauseOnCapture = !this.pauseOnCapture
+    togglePauseOnCapture() {
+      this.pauseOnCapture = !this.pauseOnCapture;
       if (!this.pauseOnCapture) {
-        this.paused = false
+        this.paused = false;
       }
     }
   }
-}
+};
 </script>
 
-<style lang="stylus">
+<style lang="sass">
 .qrcode-reader-demo 
-  padding 0px
-  .point 
-    position absolute
-    background-color red
-    border-radius 50%
-    width 10px
-    height 10px
-    margin-left -5px
-    margin-top -5px
-    transition all 40ms
-  
-  .content 
-    position absolute
-    bottom 0
-    left 0
-    right 0
-    padding 0px 20px
-    color #fff
-    font-weight bold
-    padding 10px
-    background-color rgba(0,0,0,.5)
-  
-  .controls 
-    margin-top 40px
-  
-  .error 
-    color red
-    font-weight bold
-    padding 10px
+    text-align: center;
+    padding: 0px;
 
-video 
-  opacity 0.1 
-  width 500px
+    .point 
+        position: absolute;
+        background-color: red;
+        border-radius: 50%;
+        width: 10px;
+        height: 10px;
+        margin-left: -5px;
+        margin-top: -5px;
+        transition: all 40ms;
+    
+
+    .content 
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding: 0px 20px;
+        color: #fff;
+        font-weight: bold;
+        padding: 10px;
+        background-color: rgba(0, 0, 0, 0.5);
+    
+
+    .controls 
+        margin-top: 40px;
+    
+
+    .error 
+        color: red;
+        font-weight: bold;
+        padding: 10px;
+    
+
+    video 
+        margin-top: 50px;
+        opacity: 0.7;
+        height: calc(100vh - 50px);
+    
+
 </style>
